ee4579953c4fc821dc703b6c5851c57f3ddd56f7
LeThinh <thinhlpgcc210202@fpt.edu.vn>
2025-11-29 18:48:18 +0700

    Update project code

--- admin/src/api/api.js
+++ admin/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:4000/api', // Cổng trùng với backend
+  baseURL: 'http://localhost:4000/api',
 });
 
 export default API;
